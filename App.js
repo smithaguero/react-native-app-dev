37cde7def2c58d0dccddcfc7c88a19395fa7c888
smithaguero <78059097+smithaguero@users.noreply.github.com>
2021-10-31 19:53:20 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,14 @@
-
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import App from './src/App';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const MainNavigator = createNativeStackNavigator();
-
-function LoginScreen () {
+export default function App() {
   return (
-    <View>
-    <Text>Login</Text>
+    <View style={styles.container}>
+      <Text>Open up App</Text>
+      <StatusBar style="auto" />
     </View>
-  )
-}
-
-function HomeScreen () {
-  return (
-    <View>
-    <Text>Home</Text>
-    </View>
-  )
-}
- function App() {
-return (
-  <View>
-  <NavigationContainer>
-  <MainNavigator.navigator>
-  <Navigator.Screen 
-  name = "Login" 
-  component={LoginScreen} />
-  <Navigator.Screen
-   name = "Home" 
-   component={HomeScreen} />
-  </MainNavigator.navigator>
-  </NavigationContainer>
-  </View>
-)
+  );
 }
 
 const styles = StyleSheet.create({
@@ -47,9 +19,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-export default function MyApp() {
-  return (
-    <App/>
-  );
-}
