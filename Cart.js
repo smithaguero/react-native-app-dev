9ba5577017c71e1b8c0fd955d39e2be065d9ec46
smithaguero <78059097+smithaguero@users.noreply.github.com>
2021-11-17 11:25:18 +0000

    Add files via upload

--- Cart.js
+++ Cart.js
@@ -1,59 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState,useEffect} from 'react';
-import { StyleSheet, Text,ScrollView, View,Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Feather as Icon } from '@expo/vector-icons';
-import { Icon as RNEIcon } from 'react-native-elements';
-import { useFonts } from 'expo-font';
-import { Button } from 'react-native-elements/dist/buttons/Button';
+import { CartContext } from '../CartContext';
 
-export default function Cart({navigation}){
-   return(
-      <View Style={styles.container}
-      >
-      <View Style={{flexDirection:'row'}}>
-      <View style={{ flex: 1, paddingHorizontal: 8}}>
-      
-      <Image
-        style={{ width: 100, height: 100, marginTop: 20}}
-        source={{ uri: 'https://i.pinimg.com/originals/1f/56/56/1f5656c7f84087e461a6bc991bef3b01.jpg'}}
-        />
-        <Text style={{ fontWeight: 'bold', fontSize: 20, color:'#000080' }}> Nike Men's Sportswear T-Shirt</Text>
-        
-        <View 
-        style={{
-         marginTop: 4 }}
-         >
-         <Text style={{fontFamily: 'SSBold', fontSize: 20}}> Price: 100 Ghana Cedis</Text>
-        </View>
-        <View Style={{flexDirection:'row'}}></View>
-        <View style={{flex: 1,}}>
-        <Image 
-        style={{width: 100, 
-        height: 100, 
-        marginTop: 20}}
-        source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWJsu-2-jNTbh2QMjHc01IGYwo1V2SQeOn3Q&usqp=CAU'}}
-        />
-        <Text style={{ fontWeight: 'bold', fontSize: 10}}> React native Unisex Half Sleeve T-Shirt</Text>
-        <View 
-        style={{
-         marginTop: 10 }}
-         >
-         <Text style={{ fontSize: 20}}> Price: 70 Ghana Cedis</Text>
-        </View>
-        </View>
-    </View>
-    </View>
-      </View>
-   )
+export function Cart ({navigation}) {
+    
+const {items, getItemsCount, getTotalPrice} = useContext(CartContext);
+
+  function Totals() {
+    let [total, setTotal] = useState(0);
+    useEffect(() => {
+      setTotal(getTotalPrice());
+    });
+    return (
+       <View style={styles.cartLineTotal}>
+          <Text style={[styles.lineLeft, styles.lineTotal]}>Total</Text>
+          <Text style={styles.lineRight}>$ {total}</Text>
+       </View>
+    );
+  }
+function renderItem({item}) {
+    return (
+       <View style={styles.cartLine}>
+          <Text style={styles.lineLeft}>{item.product.name} x {item.qty}</Text>
+          <Text style={styles.lineRight}>$ {item.totalPrice}</Text>
+       </View>
+    );
+  }
+
+  return (
+    <FlatList
+      style={styles.itemsList}
+      contentContainerStyle={styles.itemsListContainer}
+      data={items}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.product.id.toString()}
+      ListFooterComponent={Totals}
+    />
+  );
 }
-
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-      justifyContent: "space-between",
-      backgroundColor: "#fff",
-      padding: 20,
-      margin: 10,
-    }
-})
+  cartLine: { 
+    flexDirection: 'row',
+  },
+  cartLineTotal: { 
+    flexDirection: 'row',
+    borderTopColor: '#dddddd',
+    borderTopWidth: 1
+  },
+  lineTotal: {
+    fontWeight: 'bold',    
+  },
+  lineLeft: {
+    fontSize: 20, 
+    lineHeight: 40, 
+    color:'#333333' 
+  },
+  lineRight: { 
+    flex: 1,
+    fontSize: 20, 
+    fontWeight: 'bold',
+    lineHeight: 40, 
+    color:'#333333', 
+    textAlign:'right',
+  },
+  itemsList: {
+    backgroundColor: '#eeeeee',
+  },
+  itemsListContainer: {
+    backgroundColor: '#eeeeee',
+    paddingVertical: 8,
+    marginHorizontal: 8,
+  },
+});
